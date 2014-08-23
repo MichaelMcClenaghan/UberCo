32851e757718ab36e01e092b7d1c067fca7f320a
Sam Horn <samuel.horn1@gmail.com>
2014-08-24 01:20:07 +1000

    Cleaned up reactivation code

--- UberCoServer/reactivate.py
+++ UberCoServer/reactivate.py
@@ -3,11 +3,12 @@ import sqlite3
 db = sqlite3.connect('database.db')
 cursor = db.cursor()
 
-response = raw_input("Scan card to reactivate: (-1 to exit) ")
+while True:
+    response = raw_input('Scan card to reactivate: 1')
+    if response == '':
+        break
 
-while response != "-1":
-    cursor.execute("UPDATE card \
-                    SET valid=1 \
-                    WHERE id=" + response + ";")
-    response = raw_input("Scan card to reactivate: (-1 to exit) ")
+    cursor.execute('UPDATE card SET valid = 1 WHERE id = ?', (response,))
+    if cursor.rowcount != 1:
+        print 'Card with ID %s not found' % response
     db.commit()
