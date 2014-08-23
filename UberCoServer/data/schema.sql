7953431d14ec23d49e4d5f88b8bd6b1ca66a6959
Sam Horn <samuel.horn1@gmail.com>
2014-08-23 13:51:05 +1000

    Move database schema into its own file

---
+++ UberCoServer/data/schema.sql
@@ -0,0 +1,43 @@
+CREATE TABLE card (
+  id    INTEGER NOT NULL UNIQUE,
+  type  INTEGER,
+  valid INTEGER
+);
+
+CREATE TABLE item (
+  id          INTEGER NOT NULL UNIQUE,
+  name        TEXT,
+  description TEXT,
+  is_chest    INTEGER,
+  rarity      INTEGER,
+  image       TEXT
+);
+
+CREATE TABLE reward (
+  id              INTEGER NOT NULL UNIQUE,
+  name            TEXT,
+  description     TEXT,
+  rarity          INTEGER,
+  numberRemaining INTEGER
+);
+
+CREATE TABLE team (
+  id     INTEGER NOT NULL UNIQUE,
+  name   TEXT,
+  colour TEXT
+);
+
+CREATE TABLE team_items (
+  team_id INTEGER,
+  item_id INTEGER
+);
+
+CREATE TABLE team_rewards (
+  team_id   INTEGER,
+  reward_id INTEGER
+);
+
+CREATE TABLE chest_keys (
+  chest_id INTEGER,
+  key_id   INTEGER
+);
