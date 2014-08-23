964668520b449f5dc98fd32083e67c69b801982c
Sam Horn <samuel.horn1@gmail.com>
2014-08-24 00:34:55 +1000

    Cleaned up seed_database.py

--- UberCoServer/seed_database.py
+++ UberCoServer/seed_database.py
@@ -1,87 +1,53 @@
 import sqlite3
+import unicodecsv as csv
+from sqlite3 import DatabaseError
 
 db = sqlite3.connect('database.db')
 cursor = db.cursor()
 
-# Add data from card.csv
-card_data = open("data/cards.csv", "r")
-data = card_data.read()
+print 'Loading cards...'
+with open('data/cards.csv') as cards:
+    reader = csv.reader(cards)
+    for card in reader:
+        try:
+            cursor.execute('INSERT INTO card VALUES (?, ?, 1)', card)
+        except DatabaseError, e:
+            print 'Error adding card %s (%s)' % (card[0], e)
 
-for card in data.split("\n"):
-    card_id, card_type = card.split(',')
-    try:
-        cursor.execute("INSERT INTO card VALUES (" + card_id + "," + card_type + ",1);")
-        db.commit()
-    except:
-        print "Error adding card: ", card_id
-        db.rollback()
+print 'Loading items...'
+with open('data/items.csv') as items:
+    reader = csv.reader(items, delimiter='|')
+    for item in reader:
+        try:
+            cursor.execute('INSERT INTO item VALUES (?, ?, ?, ?, ?, ?)', item)
+        except DatabaseError, e:
+            print 'Error adding item %s (%s)' % (item[1], e)
 
-item_data = open("data/items.csv", "r")
-data = item_data.read()
+print 'Loading teams...'
+with open('data/teams.csv') as teams:
+    reader = csv.reader(teams)
+    for team in reader:
+        try:
+            cursor.execute('INSERT INTO team VALUES (?, ?, ?)', team)
+        except DatabaseError, e:
+            print 'Error adding team %s (%s)' % (team[1], e)
 
-for item in data.split("\n"):
-    try:
-        item_id, item_name, item_description, item_is_chest, item_rarity, item_image_name = item.split('|')
-    except:
-        print "Invalid line in file: " + item
+print 'Loading chest/key relationships...'
+with open('data/chest_keys.csv') as relationships:
+    reader = csv.reader(relationships)
+    for relationship in reader:
+        try:
+            cursor.execute('INSERT INTO chest_keys VALUES(?, ?)', relationship)
+        except DatabaseError, e:
+            print 'Error adding relationship %s/%s (%s): ' % (relationship[0], relationship[1], e)
 
-    try:
-        cursor.execute("INSERT INTO item VALUES (" + item_id + ",'" + item_name + "','" + \
-            item_description + "'," + item_is_chest + "," + item_rarity + ",'" + item_image_name + "');")
-        db.commit()
-    except:
-        print "Error adding item: ", item_name
-        db.rollback()
+print 'Loading rewards...'
+with open('data/rewards.csv') as rewards:
+    reader = csv.reader(rewards, delimiter='|')
+    for reward in reader:
+        try:
+            cursor.execute('INSERT INTO reward VALUES (?, ?, ?, ?, ?)', reward)
+        except DatabaseError, e:
+            print 'Error adding reward %s (%s)' % (reward[1], e)
 
-
-team_data = open("data/teams.csv", "r")
-data = team_data.read()
-
-for item in data.split("\n"):
-    try:
-        team_id, team_name, team_colour = item.split(',')
-    except:
-        print "Invalid line in file: " + item
-
-    try:
-        cursor.execute("INSERT INTO team VALUES (" + team_id + ",'" + team_name + "','" + \
-            team_colour + "');")
-        db.commit()
-    except:
-        print "Error adding team: ", team_name
-        db.rollback()
-
-relationships = open("data/chest_keys.csv", "r")
-data = relationships.read()
-
-for item in data.split("\n"):
-    try:
-        chest_id, key_id = item.split(',')
-    except:
-        print "Invalid line in file: " + item
-
-    try:
-        cursor.execute("INSERT INTO chest_keys \
-                        VALUES (" + chest_id + "," + key_id  +");")
-        db.commit()
-    except:
-        print "Error adding relationship: ", chest_id
-        db.rollback()
-
-rewards = open("data/rewards.csv", "r")
-data = rewards.read()
-
-for item in data.split("\n"):
-    try:
-        reward_id, reward_name, reward_description, reward_rarity, rewards_remaining = item.split('|')
-    except:
-        print "Invalid line in file: " + item
-
-    try:
-        cursor.execute("INSERT INTO reward \
-                        VALUES (" + reward_id + ",'" + reward_name + "','" + \
-                        reward_description + "'," + reward_rarity + "," + rewards_remaining + ");")
-        db.commit()
-    except:
-        print "Error adding reward: ", reward_name
-        db.rollback()
+db.commit()
